88510b261f6b267bb302af427bb080e0d77041f3
Алексей Волков <alek.bishop@yandex.ru>
2023-01-30 15:06:58 +0300

    edit test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -11,51 +11,50 @@ User = get_user_model()
 
 class PostModelTest(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.group = Group.objects.create(
-            title='Some group',
-            slug='some-group',
-            description='Тестовая группа'
-        )
-
     def setUp(self):
+        self.guest_client = Client()
         self.user = User.objects.create_user(username='Phantomas')
+        self.user2 = User.objects.create_user(username='Commissioner')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+        self.group = Group.objects.create(title='Тестовая группа',
+                                          slug='test_group')
+        self.post = Post.objects.create(text='Тестовый текст',
+                                        group=self.group,
+                                        author=self.user)
 
-    # def test_post_create_auth(self):
-    #     response = self.authorized_client.post(
-    #         reverse('new_post'),
-    #         data={'text': 'Nice post', 'group': PostModelTest.group.id},
-    #         follow=True,
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(Post.objects.count(), 1)
-    #     post = Post.objects.first()
-    #     self.assertEqual(post.text, 'Nice post')
-    #     self.assertEqual(post.author, self.user)
-    #     self.assertEqual(post.group.slug, PostModelTest.group.slug)
-    def test_pages_uses_correct_template(self):
+    def test_views_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
-        # Собираем в словарь пары "имя_html_шаблона: reverse(name)"
-        templates_pages_names = {
-            'posts/index.html': reverse('posts:main_paige'),
-            'posts/group_posts.html': reverse('posts:group_list',
-                                              kwargs={'slug': f'{self.group.slug}'}
-                                              ),
-            'posts/profile.html': reverse('posts:profile',
-                                          kwargs={'username': f'{self.user.username}'}
-                                          ),
-            'posts/post_detail.html': reverse('posts:post_detail',
-                                              kwargs={'post_id': self.post.id}),
-            'posts/post_create': reverse('posts:post_create'),
-            'posts/post_edit': reverse('posts:post_create',
-                                       kwargs={'post_id': self.post.id})
-        }
-        for template, reverse_name in templates_pages_names.items():
-            with self.subTest(reverse_name=reverse_name):
-                response = self.authorized_client.get(reverse_name)
+        templates_url_names = {
+            reverse('posts:main_paige'): 'posts/index.html',
+            reverse('posts:group_list',
+                    kwargs={'slug':
+                            f'{self.group.slug}'}): 'posts/group_list.html',
+            reverse('posts:profile',
+                    kwargs={'username':
+                            f'{self.user.username}'}): 'posts/profile.html',
+            reverse('posts:post_detail',
+                    kwargs={'post_id':
+                            self.post.id}): 'posts/post_detail.html',
+            reverse('posts:post_create'): 'posts/create_post.html',
+            reverse('posts:post_edit',
+                    kwargs={'post_id':
+                            self.post.id}): 'posts/create_post.html'}
+        for adress, template in templates_url_names.items():
+            with self.subTest(adress=adress):
+                response = self.authorized_client.get(adress)
                 error_name = f'Ошибка: {adress} ожидал шаблон {template}'
                 self.assertTemplateUsed(response, template, error_name)
+
+    def test_views_correct_context(self):
+        """Контекст использует соответствующий шаблон."""
+        templates_context_names = {
+            reverse('posts:main_paige'): {
+                'group': self.group,
+        }
+        }
+        for adress, context in templates_context_names.items():
+            with self.subTest(adress=adress):
+                response = self.authorized_client.get(adress)
+                error_name = f'Ошибка: {adress} ожидал контекст {context}'
+                self.assertTemplateUsed(response, 'posts/index.html', error_name)
