c412a479e846371a41a1be4222505ed19cfacbe3
Алексей Волков <alek.bishop@yandex.ru>
2023-01-31 14:28:33 +0300

    edit test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -19,7 +19,7 @@ class PostModelTest(TestCase):
         self.authorized_client.force_login(self.user)
         self.group = Group.objects.create(title='Тестовая группа',
                                           slug='test_group')
-        self.post = Post.objects.create(text='Тестовый текст',
+        self.post = Post.objects.create(text='Тестовый пост',
                                         group=self.group,
                                         author=self.user)
 
@@ -46,15 +46,50 @@ class PostModelTest(TestCase):
                 error_name = f'Ошибка: {adress} ожидал шаблон {template}'
                 self.assertTemplateUsed(response, template, error_name)
 
-    def test_views_correct_context(self):
-        """Контекст использует соответствующий шаблон."""
-        templates_context_names = {
-            reverse('posts:main_paige'): {
-                'group': self.group,
+    def test_index_page_show_correct_context(self):
+        """Шаблон index сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('posts:main_paige'))
+        first_object = response.context['page_obj'][0]
+        post_author_0 = first_object.author
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group
+        self.assertEqual(post_author_0, self.user)
+        self.assertEqual(post_text_0, self.post.text)
+        self.assertEqual(post_group_0, self.group)
+
+    def test_post_detail_page_show_correct_context(self):
+        """Шаблон post_detail сформирован с правильным контекстом."""
+        response = self.authorized_client.get(
+            reverse('posts:post_detail', kwargs={'post_id': self.post.id}))
+        post_text_0 = {response.context['posts'].text: self.post.text,
+                       response.context['posts'].group: self.group,
+                       response.context['posts'].author: self.user.username}
+        for value, expected in post_text_0.items():
+            self.assertEqual(post_text_0[value], expected)
+
+    def test_post_create_page_show_correct_context(self):
+        """Шаблон post_create сформирован с правильным контекстом."""
+        response = self.authorized_client.get(
+            reverse('posts:post_create'))
+        form_fields = {
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChoiceField
         }
-        }
-        for adress, context in templates_context_names.items():
-            with self.subTest(adress=adress):
-                response = self.authorized_client.get(adress)
-                error_name = f'Ошибка: {adress} ожидал контекст {context}'
-                self.assertTemplateUsed(response, 'posts/index.html', error_name)
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context['form'].fields.get(value)
+                self.assertIsInstance(form_field, expected)
+
+    def test_group_list_page_show_correct_context(self):
+        """Шаблон group_list сформирован с правильным контекстом."""
+        response = self.authorized_client.get(
+            reverse('posts:group_list', kwargs={'slug': f'test_slug'}))
+        first_object = response.context['page_obj'][0]
+        post_author_0 = first_object.author
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group
+        group_slug_0 = first_object.slug
+        self.assertEqual(post_author_0, self.user)
+        self.assertEqual(post_text_0, self.post.text)
+        self.assertEqual(post_group_0, self.group)
+        self.assertEqual(group_slug_0, self.group.slug)
